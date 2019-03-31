4d7b0957433bb6258f07d9dae6dcbb1a0210b800
MrTheGood <maarten@degoedeweb.nl>
2019-03-31 13:35:59 +0200

    :dash:

--- app/src/main/java/eu/insertcode/clapp/MainActivity.kt
+++ app/src/main/java/eu/insertcode/clapp/MainActivity.kt
@@ -16,16 +16,13 @@
 
 package eu.insertcode.clapp
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class SplashActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
+        setContentView(R.layout.activity_main)
     }
 }
